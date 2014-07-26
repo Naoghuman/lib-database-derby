fe991fdc0d5cc7cfc50705983ba82bda47037f1f
PRo <peter.rogge@yahoo.de>
2014-07-26 07:05:02 +0200

    Add JavaDoc to the packages.

---
+++ src/main/java/de/pro/lib/database/api/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Contains the <code>API</code> for the library <code>Lib-Database</code>.
+ */
+package de.pro.lib.database.api;
---
+++ src/main/java/de/pro/lib/database/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Contains the implementation for the library <code>Lib-Database</code>.
+ */
+package de.pro.lib.database;
